4062a3f9ea6160bbb0475c7a6f8746564332e1c8
burcuoksuz7 <67266830+burcuoksuz7@users.noreply.github.com>
2021-06-13 20:50:02 +0300

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,262 @@
+﻿
+namespace AVMKatPlan
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.pictureBox2 = new System.Windows.Forms.PictureBox();
+			this.groupBox1 = new System.Windows.Forms.GroupBox();
+			this.checkBox2 = new System.Windows.Forms.CheckBox();
+			this.checkBox1 = new System.Windows.Forms.CheckBox();
+			this.btnBgr = new System.Windows.Forms.Button();
+			this.btnKtn = new System.Windows.Forms.Button();
+			this.btnLcw = new System.Windows.Forms.Button();
+			this.btnStb = new System.Windows.Forms.Button();
+			this.lblSoru = new System.Windows.Forms.Label();
+			this.groupBox2 = new System.Windows.Forms.GroupBox();
+			this.lblYol3 = new System.Windows.Forms.Label();
+			this.lblYol2 = new System.Windows.Forms.Label();
+			this.lblYol1 = new System.Windows.Forms.Label();
+			this.lblSecim = new System.Windows.Forms.Label();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+			this.groupBox1.SuspendLayout();
+			this.groupBox2.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+			this.pictureBox1.Location = new System.Drawing.Point(1, 0);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(485, 576);
+			this.pictureBox1.TabIndex = 0;
+			this.pictureBox1.TabStop = false;
+			// 
+			// pictureBox2
+			// 
+			this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+			this.pictureBox2.Location = new System.Drawing.Point(487, 0);
+			this.pictureBox2.Name = "pictureBox2";
+			this.pictureBox2.Size = new System.Drawing.Size(485, 576);
+			this.pictureBox2.TabIndex = 1;
+			this.pictureBox2.TabStop = false;
+			// 
+			// groupBox1
+			// 
+			this.groupBox1.Controls.Add(this.lblSecim);
+			this.groupBox1.Controls.Add(this.checkBox2);
+			this.groupBox1.Controls.Add(this.checkBox1);
+			this.groupBox1.Controls.Add(this.btnBgr);
+			this.groupBox1.Controls.Add(this.btnKtn);
+			this.groupBox1.Controls.Add(this.btnLcw);
+			this.groupBox1.Controls.Add(this.btnStb);
+			this.groupBox1.Controls.Add(this.lblSoru);
+			this.groupBox1.Location = new System.Drawing.Point(1, 571);
+			this.groupBox1.Name = "groupBox1";
+			this.groupBox1.Size = new System.Drawing.Size(971, 147);
+			this.groupBox1.TabIndex = 13;
+			this.groupBox1.TabStop = false;
+			// 
+			// checkBox2
+			// 
+			this.checkBox2.AutoSize = true;
+			this.checkBox2.Enabled = false;
+			this.checkBox2.Location = new System.Drawing.Point(487, 123);
+			this.checkBox2.Name = "checkBox2";
+			this.checkBox2.Size = new System.Drawing.Size(70, 17);
+			this.checkBox2.TabIndex = 19;
+			this.checkBox2.Text = "Merdiven";
+			this.checkBox2.UseVisualStyleBackColor = true;
+			this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged_1);
+			// 
+			// checkBox1
+			// 
+			this.checkBox1.AutoSize = true;
+			this.checkBox1.Enabled = false;
+			this.checkBox1.Location = new System.Drawing.Point(401, 123);
+			this.checkBox1.Name = "checkBox1";
+			this.checkBox1.Size = new System.Drawing.Size(64, 17);
+			this.checkBox1.TabIndex = 18;
+			this.checkBox1.Text = "Asansör";
+			this.checkBox1.UseVisualStyleBackColor = true;
+			this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged_1);
+			// 
+			// btnBgr
+			// 
+			this.btnBgr.Font = new System.Drawing.Font("Candara", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.btnBgr.Image = ((System.Drawing.Image)(resources.GetObject("btnBgr.Image")));
+			this.btnBgr.Location = new System.Drawing.Point(558, 49);
+			this.btnBgr.Name = "btnBgr";
+			this.btnBgr.Size = new System.Drawing.Size(67, 68);
+			this.btnBgr.TabIndex = 17;
+			this.btnBgr.UseVisualStyleBackColor = true;
+			this.btnBgr.Click += new System.EventHandler(this.btnBgr_Click_1);
+			// 
+			// btnKtn
+			// 
+			this.btnKtn.Font = new System.Drawing.Font("Candara", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.btnKtn.Image = ((System.Drawing.Image)(resources.GetObject("btnKtn.Image")));
+			this.btnKtn.Location = new System.Drawing.Point(487, 49);
+			this.btnKtn.Name = "btnKtn";
+			this.btnKtn.Size = new System.Drawing.Size(67, 68);
+			this.btnKtn.TabIndex = 16;
+			this.btnKtn.UseVisualStyleBackColor = true;
+			this.btnKtn.Click += new System.EventHandler(this.btnKtn_Click_1);
+			// 
+			// btnLcw
+			// 
+			this.btnLcw.Font = new System.Drawing.Font("Candara", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.btnLcw.Image = ((System.Drawing.Image)(resources.GetObject("btnLcw.Image")));
+			this.btnLcw.Location = new System.Drawing.Point(416, 49);
+			this.btnLcw.Name = "btnLcw";
+			this.btnLcw.Size = new System.Drawing.Size(67, 68);
+			this.btnLcw.TabIndex = 15;
+			this.btnLcw.UseVisualStyleBackColor = true;
+			this.btnLcw.Click += new System.EventHandler(this.btnLcw_Click_1);
+			// 
+			// btnStb
+			// 
+			this.btnStb.Font = new System.Drawing.Font("Candara", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.btnStb.Image = ((System.Drawing.Image)(resources.GetObject("btnStb.Image")));
+			this.btnStb.Location = new System.Drawing.Point(345, 49);
+			this.btnStb.Name = "btnStb";
+			this.btnStb.Size = new System.Drawing.Size(67, 68);
+			this.btnStb.TabIndex = 14;
+			this.btnStb.UseVisualStyleBackColor = true;
+			this.btnStb.Click += new System.EventHandler(this.btnStb_Click_1);
+			// 
+			// lblSoru
+			// 
+			this.lblSoru.AutoSize = true;
+			this.lblSoru.Font = new System.Drawing.Font("Candara", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.lblSoru.Location = new System.Drawing.Point(319, 13);
+			this.lblSoru.Name = "lblSoru";
+			this.lblSoru.Size = new System.Drawing.Size(328, 26);
+			this.lblSoru.TabIndex = 13;
+			this.lblSoru.Text = "Gitmek istediğiniz mağazayı seçiniz";
+			// 
+			// groupBox2
+			// 
+			this.groupBox2.Controls.Add(this.lblYol3);
+			this.groupBox2.Controls.Add(this.lblYol2);
+			this.groupBox2.Controls.Add(this.lblYol1);
+			this.groupBox2.Location = new System.Drawing.Point(1, 717);
+			this.groupBox2.Name = "groupBox2";
+			this.groupBox2.Size = new System.Drawing.Size(971, 114);
+			this.groupBox2.TabIndex = 20;
+			this.groupBox2.TabStop = false;
+			// 
+			// lblYol3
+			// 
+			this.lblYol3.AutoSize = true;
+			this.lblYol3.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.lblYol3.Location = new System.Drawing.Point(12, 79);
+			this.lblYol3.Name = "lblYol3";
+			this.lblYol3.Size = new System.Drawing.Size(41, 18);
+			this.lblYol3.TabIndex = 12;
+			this.lblYol3.Text = "label";
+			// 
+			// lblYol2
+			// 
+			this.lblYol2.AutoSize = true;
+			this.lblYol2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.lblYol2.Location = new System.Drawing.Point(12, 51);
+			this.lblYol2.Name = "lblYol2";
+			this.lblYol2.Size = new System.Drawing.Size(41, 18);
+			this.lblYol2.TabIndex = 11;
+			this.lblYol2.Text = "label";
+			// 
+			// lblYol1
+			// 
+			this.lblYol1.AutoSize = true;
+			this.lblYol1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.lblYol1.Location = new System.Drawing.Point(12, 23);
+			this.lblYol1.Name = "lblYol1";
+			this.lblYol1.Size = new System.Drawing.Size(41, 18);
+			this.lblYol1.TabIndex = 10;
+			this.lblYol1.Text = "label";
+			// 
+			// lblSecim
+			// 
+			this.lblSecim.AutoSize = true;
+			this.lblSecim.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.lblSecim.ForeColor = System.Drawing.Color.Red;
+			this.lblSecim.Location = new System.Drawing.Point(563, 123);
+			this.lblSecim.Name = "lblSecim";
+			this.lblSecim.Size = new System.Drawing.Size(117, 15);
+			this.lblSecim.TabIndex = 13;
+			this.lblSecim.Text = "Lütfen bir yol seçiniz";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+			this.BackColor = System.Drawing.Color.White;
+			this.ClientSize = new System.Drawing.Size(973, 829);
+			this.Controls.Add(this.groupBox2);
+			this.Controls.Add(this.groupBox1);
+			this.Controls.Add(this.pictureBox2);
+			this.Controls.Add(this.pictureBox1);
+			this.Name = "Form1";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "AVM Yönlendirme Planı";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+			this.groupBox1.ResumeLayout(false);
+			this.groupBox1.PerformLayout();
+			this.groupBox2.ResumeLayout(false);
+			this.groupBox2.PerformLayout();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.PictureBox pictureBox2;
+		private System.Windows.Forms.GroupBox groupBox1;
+		private System.Windows.Forms.CheckBox checkBox2;
+		private System.Windows.Forms.CheckBox checkBox1;
+		private System.Windows.Forms.Button btnBgr;
+		private System.Windows.Forms.Button btnKtn;
+		private System.Windows.Forms.Button btnLcw;
+		private System.Windows.Forms.Button btnStb;
+		private System.Windows.Forms.Label lblSoru;
+		private System.Windows.Forms.GroupBox groupBox2;
+		private System.Windows.Forms.Label lblYol3;
+		private System.Windows.Forms.Label lblYol2;
+		private System.Windows.Forms.Label lblYol1;
+		private System.Windows.Forms.Label lblSecim;
+	}
+}
+
---
+++ Form1.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AVMKatPlan
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		bool secim = false;
+		
+		private void btnStb_Click_1(object sender, EventArgs e)
+		{
+
+			lblYol1.Visible = true;
+			lblYol2.Visible = true;
+			lblYol3.Visible = true;
+
+			lblSecim.Visible = false;
+
+			checkBox1.Checked = false;
+			checkBox2.Checked = false;
+
+			checkBox1.Enabled = false;
+			checkBox2.Enabled = false;
+
+			lblYol1.ForeColor = System.Drawing.Color.Green;
+			lblYol2.ForeColor = System.Drawing.Color.Green;
+			lblYol3.ForeColor = System.Drawing.Color.Green;
+			//3 FARKLI KİOKS İÇİN 3 FARKLI LABEL BELİRLEDİK VE 3 FARKLI YOLDAN GİDİŞ SEÇENEĞİ SUNDUK
+
+			lblYol1.Text = "Eğer 1. Kiokstaysanız >> E1-D1-D2-D3";
+			lblYol2.Text = "Eğer 2. Kiokstaysanız >> K4-K3-J3-I3-H3-G3-F3-E3-D4";
+			lblYol3.Text = "Eğer 3. Kiokstaysanız >> E11-D11-D10-D9-D8-D7-D6-D5-D4-D3";
+		}
+
+		private void btnLcw_Click_1(object sender, EventArgs e)
+		{
+			lblYol1.Visible = true;
+			lblYol2.Visible = true;
+			lblYol3.Visible = true;
+
+			lblSecim.Visible = false;
+
+			checkBox1.Checked = false;
+			checkBox2.Checked = false;
+
+			checkBox1.Enabled = false;
+			checkBox2.Enabled = false;
+
+			lblYol1.ForeColor = System.Drawing.Color.Blue;
+			lblYol2.ForeColor = System.Drawing.Color.Blue;
+			lblYol3.ForeColor = System.Drawing.Color.Blue;
+
+			lblYol1.Text = "Eğer 1. Kiokstaysanız >> E1-D1-C1-B1-B2-B3-B4-B5-B6-B7";
+			lblYol2.Text = "Eğer 2. Kiokstaysanız >> K6-K7-J7-I7-H7-G7-F7-E7-D7-C7-B7";
+			lblYol3.Text = "Eğer 3. Kiokstaysanız >> E11-D11-C11-B11-B10";
+		}
+
+		private void btnKtn_Click_1(object sender, EventArgs e)
+		{
+			lblYol1.Visible = true;
+			lblYol2.Visible = true;
+			lblYol3.Visible = true;
+
+			lblSecim.Visible = false;
+
+			checkBox1.Checked = false;
+			checkBox2.Checked = false;
+
+			checkBox1.Enabled = false;
+			checkBox2.Enabled = false;
+
+			lblYol1.ForeColor = System.Drawing.Color.Black;
+			lblYol2.ForeColor = System.Drawing.Color.Black;
+			lblYol3.ForeColor = System.Drawing.Color.Black;
+
+			lblYol1.Text = "Eğer 1. Kiokstaysanız >> F2-F3-F4-F5-F6-F7-G7-H7";
+			lblYol2.Text = "Eğer 2. Kiokstaysanız >> K6-K7-J7-I7-H7";
+			lblYol3.Text = "Eğer 3. Kiokstaysanız >>G11-H11-H10-H9-H8-H7";
+		}
+
+		private void btnBgr_Click_1(object sender, EventArgs e)
+		{
+			lblSecim.Visible = true;
+
+			checkBox1.Enabled = true;
+			checkBox2.Enabled = true;
+			checkBox1.ForeColor = System.Drawing.Color.Red;
+			checkBox2.ForeColor = System.Drawing.Color.Red;
+			lblYol1.Visible = false;
+			lblYol2.Visible = false;
+			lblYol3.Visible = false;
+		}
+
+		private void checkBox1_CheckedChanged_1(object sender, EventArgs e)
+		{
+			lblSecim.Visible = true;
+
+			lblYol1.Visible = true;
+			lblYol2.Visible = true;
+			lblYol3.Visible = true;
+
+			if (!secim)
+			{
+				if (checkBox2.Checked)
+				{
+					secim = true;
+					checkBox2.Checked = false;
+				}
+			}
+			else
+			{
+				secim = false;
+			}
+
+			lblYol1.ForeColor = System.Drawing.Color.Red;
+			lblYol2.ForeColor = System.Drawing.Color.Red;
+			lblYol3.ForeColor = System.Drawing.Color.Red;
+
+			lblYol1.Text = "Eğer 1. Kiokstaysanız >> E1-E2-E3-E4-E5- Asansöre ulaştınız -D5-C5-C6";
+			lblYol2.Text = "Eğer 2. Kiokstaysanız >> K5-J5-I5-H5-G5-F5-E5- Asansöre ulaştınız -D5-C5-C6";
+			lblYol3.Text = "Eğer 3. kiokstaysanız >> E11-E10-E9-E8-E7-E6-E5- Asansöre ulaştınız -D5-C5-C6";
+		}
+
+		private void checkBox2_CheckedChanged_1(object sender, EventArgs e)
+		{
+			lblSecim.Visible = true;
+
+			lblYol1.Visible = true;
+			lblYol2.Visible = true;
+			lblYol3.Visible = true;
+
+			if (!secim)
+			{
+				if (checkBox1.Checked)
+				{
+					secim = true;
+					checkBox1.Checked = false;
+				}
+			}
+			else
+			{
+				secim = false;
+			}
+
+			lblYol1.ForeColor = System.Drawing.Color.Red;
+			lblYol2.ForeColor = System.Drawing.Color.Red;
+			lblYol3.ForeColor = System.Drawing.Color.Red;
+
+			lblYol1.Text = "Eğer 1. Kiokstaysanız >> F2-F3-F4-F5-E5-D5-C5-B5-A5- Merdivene ulaştınız -A6-B6-C6";
+			lblYol2.Text = "Eğer 2. Kiokstaysanız >> J5-I5-H5-G5-F5-E5-D5-C5-B5-A5- Merdivene ulaştınız -A6-B6-C6";
+			lblYol3.Text = "Eğer 3. kiokstaysanız >> E11-D11-C11-A11-A10-A9-A8-A7-A6-A5- Merdivene ulaştınız -A6-B6-C6";
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			lblYol1.Visible = false;
+			lblYol2.Visible = false;
+			lblYol3.Visible = false;
+			lblSecim.Visible = false;
+			checkBox1.Enabled = false;
+			checkBox2.Enabled = false;
+			checkBox1.Checked = false;
+			checkBox2.Checked = false;
+			
+		}
+
+		//KULLANICI HANGİ MAĞAZAYA GİTMEK İSTİYORSA O MAĞAZAYI SEÇER VE EKRANA GELEN YOLLARI İZLER.
+
+
+
+
+	}
+}
